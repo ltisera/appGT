70dbe7632c67ff5ee919e8e127567757a47ee5a6
rociofretes <rociobh80@gmail.com>
2022-10-24 03:23:37 -0300

    icon android

--- appGT/app/src/main/java/com/example/appgt/LoginActivity.java
+++ appGT/app/src/main/java/com/example/appgt/LoginActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -61,5 +63,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
 
 }
--- appGT/app/src/main/java/com/example/appgt/MainActivity.java
+++ appGT/app/src/main/java/com/example/appgt/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -57,5 +59,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 
 }
